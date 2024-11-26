03c6fd0e647127d5ecbf6a2020ca6f6cdd163e7b
Alvin Ichi <48800728+nhatlapross@users.noreply.github.com>
2024-11-26 19:55:36 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+          'utf-8-validate': 'commonjs utf-8-validate',
+          'bufferutil': 'commonjs bufferutil',
+        });
+        return config;
+      },
+      // Ensure client-side only rendering
+      async redirects() {
+        return [
+          {
+            source: '/server-side-page',
+            destination: '/',
+            permanent: true,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
